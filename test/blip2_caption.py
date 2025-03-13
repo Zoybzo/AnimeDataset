0b498fc3d8aae0e4386c439ff3166f3c13bfbf32
Mkpk <user@BJ206116.local>
2025-03-13 14:39:41 +0800

    Update blip2

--- test/blip2_caption.py
+++ test/blip2_caption.py
@@ -4,9 +4,10 @@ from transformers import Blip2Processor, Blip2ForConditionalGeneration
 
 from utils.get_path import MHOME
 
-model_path = f"{MHOME}/Models/blip2-opt-2.7b"
-processor = Blip2Processor.from_pretrained(model_path)
-model = Blip2ForConditionalGeneration.from_pretrained(model_path)
+# model_path = f"{MHOME}/Models/blip2-opt-2.7b"
+model_path = "Salesforce/blip2-opt-2.7b"
+processor = Blip2Processor.from_pretrained(model_path, local_files_only=True)
+model = Blip2ForConditionalGeneration.from_pretrained(model_path, local_files_only=True)
 
 image_path = "./assets/test/naruto.jpeg"
 # img_url = 'https://storage.googleapis.com/sfr-vision-language-research/BLIP/demo.jpg' 
