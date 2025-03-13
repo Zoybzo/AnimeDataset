53f7f2d70632c3ca0dbedac1b086bac45c89a9ea
zoybzo <hadequing@gmail.com>
2025-03-13 17:14:08 +0800

    cogview4

--- test/cogview4_generation.py
+++ test/cogview4_generation.py
@@ -4,9 +4,9 @@ import torch
 pipe = CogView4Pipeline.from_pretrained("THUDM/CogView4-6B", torch_dtype=torch.bfloat16, local_files_only=True, device_map="balanced")
 
 # Open it for reduce GPU memory usage
-pipe.enable_model_cpu_offload()
-pipe.vae.enable_slicing()
-pipe.vae.enable_tiling()
+# pipe.enable_model_cpu_offload()
+# pipe.vae.enable_slicing()
+# pipe.vae.enable_tiling()
 
 # prompt = "A vibrant cherry red sports car sits proudly under the gleaming sun, its polished exterior smooth and flawless, casting a mirror-like reflection. The car features a low, aerodynamic body, angular headlights that gaze forward like predatory eyes, and a set of black, high-gloss racing rims that contrast starkly with the red. A subtle hint of chrome embellishes the grille and exhaust, while the tinted windows suggest a luxurious and private interior. The scene conveys a sense of speed and elegance, the car appearing as if it's about to burst into a sprint along a coastal road, with the ocean's azure waves crashing in the background."
 
