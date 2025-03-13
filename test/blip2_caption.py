6a8288da10ff653d2ea451b76e4f48ea7a9dc263
zoybzo <hadequing@gmail.com>
2025-03-13 14:46:02 +0800

    Update blip2

--- test/blip2_caption.py
+++ test/blip2_caption.py
@@ -7,14 +7,14 @@ from utils.get_path import MHOME
 # model_path = f"{MHOME}/Models/blip2-opt-2.7b"
 model_path = "Salesforce/blip2-opt-2.7b"
 processor = Blip2Processor.from_pretrained(model_path, local_files_only=True)
-model = Blip2ForConditionalGeneration.from_pretrained(model_path, local_files_only=True)
+model = Blip2ForConditionalGeneration.from_pretrained(model_path, device_map="auto", local_files_only=True)
 
 img_path = "./assets/test/naruto.jpeg"
 # img_url = 'https://storage.googleapis.com/sfr-vision-language-research/BLIP/demo.jpg' 
 raw_image = Image.open(img_path).convert('RGB')
 
-question = "describe this image in detail"
-inputs = processor(raw_image, question, return_tensors="pt")
+question = "Could you describe this image in detail?"
+inputs = processor(raw_image, question, return_tensors="pt").to("cuda")
 
 print("generating describe")
 out = model.generate(**inputs)
