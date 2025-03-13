2fd9d199916153a7ff281abbae0eac2224298fff
zoybzo <hadequing@gmail.com>
2025-03-13 17:11:45 +0800

    add trainer template

---
+++ trainers/Trainer.py
@@ -0,0 +1,17 @@
+class Trainer:
+    def __init__(self, args):
+        pass
+
+    def prepare_models(self):
+        pass
+
+    def train(self,):
+        pass
+
+    def validate(self):
+        pass
+
+    def fit(self):
+        pass
+
+
