204f8791adfc1bfe6329e452295b950c6289a2fe
zoybzo <hadequing@gmail.com>
2025-03-17 17:22:26 +0800

    Test vae psnr

--- test/vae.py
+++ test/vae.py
@@ -2,6 +2,9 @@ from diffusers import CogView4Pipeline, AutoencoderKL
 from torchvision import transforms
 import torch
 from PIL import Image
+from skimage.metrics import peak_signal_noise_ratio
+import numpy as np
+
 
 from loguru import logger
 
@@ -17,6 +20,7 @@ vae = AutoencoderKL.from_pretrained(
 
 # load image
 img_path = "./assets/test/naruto.jpeg"
+result_img_path = "./assets/test/vae_naruto.png"
 raw_image = Image.open(img_path).convert("RGB")
 logger.debug(f"raw_image: {raw_image.size}")
 # 创建转换器对象
@@ -32,4 +36,10 @@ logger.debug(f"sample: {sample}")
 sample = sample["sample"].to(dtype=torch.float32).squeeze(0)
 logger.debug(f"sample.shape: {sample.shape}")
 rec_image = to_pil_image(sample)
-rec_image.save("./assets/test/vae_naruto.png")
+rec_image.save(result_img_path)
+
+
+img1 = np.array(Image.open(img_path).convert("L"), dtype=np.float32)
+img2 = np.array(Image.open(result_img_path).convert("L"), dtype=np.float32)
+psnr_value = peak_signal_noise_ratio(img1, img2, data_range=255)
+logger.info(f"PSNR: {psnr_value:.2f} dB")
