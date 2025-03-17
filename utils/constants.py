868ce4afc96f166b9f2597b1ed9bf7e35e6e75a3
zoybzo <hadequing@gmail.com>
2025-03-17 16:47:41 +0800

    Added prompts

---
+++ utils/constants.py
@@ -0,0 +1,14 @@
+PROMPT = {
+    "chainsaw_man": {
+        "en": "2people, denji, chainsaw man, makima, chainsaw man, denji, casualclothes, makima,businesssuit, shopping, carrying bags, walking side by side, city street, daytime, v, masterpiece, high score, great score, absurdres",
+        "zh": "2人,电次,电锯人,玛奇玛,电锯人,电次,休闲服装,玛奇玛,商务服装,购物,背着背包,并肩走路,商业街,白天,v,masterpiece, high score, great score, absurdres",
+    },
+    "naruto": {
+        "en": "2boys, uzumaki naruto, naruto, uchiha sasuke, naruto, uzumaki naruto,(orange jumpsuit), uchiha sasuke,(uchiha clan outfit), intense fighting, clashing, dynamic pose, determined expression, valley of the end, waterfall, stone statues, sunset, v, masterpiece, high score, great score, absurdres",
+        "zh": "2个男人,漩涡鸣人,火影忍者,宇智波佐助,火影忍者,漩涡鸣人,橘色服装,宇智波佐助,宇智波服装,激烈的战斗,冲突,动态姿势,坚决的表情,终结谷,瀑布,石头雕像,日落,v,masterpiece,high score,great score, absurdres",
+    },
+    "demon_slayer": {
+        "en": "2people, kamado nezuko, demon slayer, kamado tanjiro, demon slayer, kamado nezuko,demonform, kamado tanjiro, waterhashirauniform, practicing fighting, bamboo forest, dynamic pose, determined expression, daytime, v, masterpiece, high score, great score, absurdres",
+        "zh": "2个人,灶门弥豆子,鬼灭之刃,灶门炭治郎,鬼灭之刃,灶门弥豆子,鬼的衣服,灶门炭治郎,waterhashirauniform,练习战斗,竹林,动态姿势,坚决的表情,白天,v,masterpiece,high score, great score, absurdres",
+    },
+}
