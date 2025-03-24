b7ebba62b172df326597c221686a52714196023d
zoybzo <hadequing@gmail.com>
2025-03-24 14:15:13 +0800

    Added data crawler

--- data/waifucc.py
+++ data/waifucc.py
@@ -1,52 +1,17 @@
-from waifuc.action import (
-    NoMonochromeAction,
-    FilterSimilarAction,
-    TaggingAction,
-    PaddingAlignAction,
-    PersonSplitAction,
-    FaceCountAction,
-    FirstNSelectAction,
-    CCIPAction,
-    ModeConvertAction,
-    ClassFilterAction,
-    RandomFilenameAction,
-    AlignMinSizeAction,
-)
-from waifuc.export import TextualInversionExporter
-from waifuc.source import GcharAutoSource
+from waifuc.action import HeadCountAction, AlignMinSizeAction
+from waifuc.export import SaveExporter
+from waifuc.source import DanbooruSource
 
 if __name__ == "__main__":
-    # data source for surtr in arknights, images from many sites will be crawled
-    # all supported games and sites can be found at
-    # https://narugo1992.github.io/gchar/main/best_practice/supported/index.html#supported-games-and-sites
-    # ATTENTION: GcharAutoSource required `git+https://github.com/deepghs/waifuc.git@main#egg=waifuc[gchar]`
-    s = GcharAutoSource("star rail kafka")
-
-    # crawl images, process them, and then save them to directory with given format
-    s.attach(
-        # preprocess images with white background RGB
-        ModeConvertAction("RGB", "white"),
-        # pre-filtering for images
-        NoMonochromeAction(),  # no monochrome, greyscale or sketch
-        ClassFilterAction(["illustration", "bangumi"]),  # no comic or 3d
-        # RatingFilterAction(['safe', 'r15']),  # filter images with rating, like safe, r15, r18
-        FilterSimilarAction("all"),  # filter duplicated images
-        # human processing
-        FaceCountAction(1),  # drop images with 0 or >1 faces
-        PersonSplitAction(),  # crop for each person
-        FaceCountAction(1),
-        # CCIP, filter the character you may not want to see in dataset
-        CCIPAction(min_val_count=15),
-        # if min(height, weight) > 800, resize it to 800
-        AlignMinSizeAction(800),
-        # tagging with wd14 v2, if you don't need character tag, set character_threshold=1.01
-        TaggingAction(force=True),
-        PaddingAlignAction((512, 512)),  # align to 512x512
-        FilterSimilarAction("all"),  # filter again
-        FirstNSelectAction(200),  # first 200 images
-        # MirrorAction(),  # mirror image for data augmentation
-        RandomFilenameAction(ext=".png"),  # random rename files
-    ).export(
-        # save to surtr_dataset directory
-        TextualInversionExporter("kafka_waifucc_dataset")
+    source = DanbooruSource(["kafka_(star rail)", "solo"])
+    source.attach(
+        # only 1 head,
+        HeadCountAction(1),
+        # if shorter side is over 640, just resize it to 640
+        AlignMinSizeAction(640),
+    )[
+        :10
+    ].export(  # only first 10 images
+        # save images (with meta information from danbooru site)
+        SaveExporter("/data/kafka_waifu_dataset")
     )
