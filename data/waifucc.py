72df3d5a0dd863f3f25ff1d0bb8510d96f558c42
zoybzo <hadequing@gmail.com>
2025-03-24 14:21:16 +0800

    Added data crawler

--- data/waifucc.py
+++ data/waifucc.py
@@ -8,15 +8,10 @@ from utils.get_path import HOME
 
 if __name__ == "__main__":
     source = DanbooruSource(["kafka_(star rail)", "solo"])
+    save_path = os.path.join(HOME, "Datasets/kafka_waifu_dataset")
     source.attach(
         # only 1 head,
         HeadCountAction(1),
         # if shorter side is over 640, just resize it to 640
         AlignMinSizeAction(640),
-    )[
-        :
-    ].export(  # only first 10 images
-        # save images (with meta information from danbooru site)
-        save_path = os.path.join(f"{HOME}", "Datasets/kafka_waifu_dataset")
-        SaveExporter(save_path)
-    )
+    )[:].export(SaveExporter(save_path))
