24d35a2b2b027fdf8adc5d16f9af7525d6856a5a
zoybzo <hadequing@gmail.com>
2025-03-27 15:03:04 +0800

    test vae with psnr

--- trainers/VaeTester.py
+++ trainers/VaeTester.py
@@ -21,23 +21,23 @@ class VaeTester(Trainer):
     def prepare_models(
         self, model_path=None, subfolder=None, device="cpu", dtype=torch.float32
     ):
-        # if "stable" in model_path:
-        #     pipe = DiffusionPipeline.from_pretrained(
-        #         model_path,
-        #         torch_dtype=dtype,
-        #         use_safetensors=True,
-        #     )
-        #     pipe.to(self.device)
-        #     self.vae = pipe.vae
-        # else:
-        self.vae = AutoencoderKL.from_pretrained(
-            model_path,
-            torch_dtype=dtype,
-            local_files_only=True,
-            subfolder=subfolder,
-        ).to(device)
-        self.vae.enable_slicing()
-        self.vae.enable_tiling()
+        if "stable" in model_path:
+            pipe = DiffusionPipeline.from_pretrained(
+                model_path,
+                torch_dtype=dtype,
+                use_safetensors=True,
+            )
+            pipe.to(self.device)
+            self.vae = pipe.vae
+        else:
+            self.vae = AutoencoderKL.from_pretrained(
+                model_path,
+                torch_dtype=dtype,
+                local_files_only=True,
+                subfolder=subfolder,
+            ).to(device)
+            self.vae.enable_slicing()
+            self.vae.enable_tiling()
 
     @torch.no_grad()
     def validate(self, dataloader):
