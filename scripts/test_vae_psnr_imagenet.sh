3edc9d37e598a5b83eb66dd84694399d659ff065
zoybzo <hadequing@gmail.com>
2025-03-27 14:44:16 +0800

    test vae with psnr

--- scripts/test_vae_psnr_imagenet.sh
+++ scripts/test_vae_psnr_imagenet.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 python vae_psnr.py \
-    --model_path "{$MODEL_HOME}/stable-diffusion-xl-base-1.0" \
+    --model_path "$MODEL_HOME/stable-diffusion-xl-base-1.0" \
     --dataset "ImageNet" \
     --device "cuda:2"
 
--- scripts/test_vae_psnr_mirai.sh
+++ scripts/test_vae_psnr_mirai.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 python vae_psnr.py \
-  --model_path "{$MODEL_HOME}/stable-diffusion-xl-base-1.0"
+  --model_path "$MODEL_HOME/stable-diffusion-xl-base-1.0" \
+  --dataset "mirai" \
+  --device "cuda:0"
+
